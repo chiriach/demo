d06d20127a1b2dbbd67cf81ae2db5a01228c0df0
Victor Carabulea <carabuleavictor26@gmail.com>
2025-10-27 14:04:49 +0200

    Getter and Setter SecurityStaff

--- src/main/java/com/example/MallManagement/model/SecurityStaff.java
+++ src/main/java/com/example/MallManagement/model/SecurityStaff.java
@@ -2,9 +2,20 @@ package com.example.MallManagement.model;
 
 public class SecurityStaff extends Staff {
 
-    public String badgeNo;
+    private String badgeNo;
+
     SecurityStaff(String id, String name, String badgeNo) {
         super(id, name);
         this.badgeNo = badgeNo;
     }
+
+    public String getBadgeNo() {
+        return badgeNo;
+    }
+
+    public void setBadgeNo(String badgeNo) {
+        this.badgeNo = badgeNo;
+    }
+
+
 }
