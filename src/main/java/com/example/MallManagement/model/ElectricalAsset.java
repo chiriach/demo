89d6d8ff253bb10915668acb4b124320edfd8fa2
chiri <andrei.chiriac4@yahoo.com>
2025-10-27 14:05:17 +0200

    ElectricalAsset Model

---
+++ src/main/java/com/example/MallManagement/model/ElectricalAsset.java
@@ -0,0 +1,47 @@
+package com.example.MallManagement.model;
+
+public class ElectricalAsset{
+    private String id;
+    private String floorId;
+    private String type;
+    private String status;
+
+    public ElectricalAsset(String id, String floorId, String type, String status) {
+        this.id = id;
+        this.floorId = floorId;
+        this.type = type;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFloorId() {
+        return floorId;
+    }
+
+    public void setFloorId(String floorId) {
+        this.floorId = floorId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
