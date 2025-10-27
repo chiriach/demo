7055726327bed98538e98928ae01aea49fb7eb74
Victor Carabulea <carabuleavictor26@gmail.com>
2025-10-27 14:04:40 +0200

    Getter and Setter Purchase

--- src/main/java/com/example/MallManagement/model/Purchase.java
+++ src/main/java/com/example/MallManagement/model/Purchase.java
@@ -2,13 +2,45 @@ package com.example.MallManagement.model;
 
 public class Purchase {
 
-    public String id, customerId, shopId;
-    public double amount;
+    private String id, customerId, shopId;
+    private double amount;
 
     Purchase(String id, String customerId, String shopId, double amount){
         this.id = id;
         this.customerId = customerId;
         this.shopId = shopId;
         this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(String shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
     }
 }
